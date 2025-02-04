ebfc27ec660caa9c60a4b76afce2432c65f315c9
locnvhome <nguyenvanlocmmo@gmail.com>
2025-02-05 06:53:57 +0700

    Git fist init project commit from home

---
+++ src/main/java/com/example/fullstep/java_backend_fullstep/JavaBackendFullstepApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstep.java_backend_fullstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendFullstepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendFullstepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fullstep/java_backend_fullstep/JavaBackendFullstepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstep.java_backend_fullstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendFullstepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
